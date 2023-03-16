b5e84424391a4da859c1bd12182e324ae9781c32
Christian <macheadway@gmail.com>
2023-03-16 07:54:14 -0700

    4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main  - Prints "Programming is like building a multilingual puzzle,"
+ * Return: 0
+ */
+int main(void)
+{
+        printf("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
