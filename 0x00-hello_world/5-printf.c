3a3680f5f1db4e89480cadaa0307d71d98b81c8c
Christian <macheadway@gmail.com>
2023-03-16 07:20:54 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-printf "with proper grammar, but the outcome is a piece of art,"
+#include <stdio.h>
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
