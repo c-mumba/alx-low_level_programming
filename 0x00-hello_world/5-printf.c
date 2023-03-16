55edbe78d5b28107c63c57013cabdacc1846919d
Christian <macheadway@gmail.com>
2023-03-16 07:45:06 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
- * a blank line
- * Description: Longer description of the function)?
- * section header: Section description)*
- * Return: Description of the returned value
+ * main  - Prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
  */
 int main(void)
 {
