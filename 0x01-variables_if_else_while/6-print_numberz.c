5ae1428f2a739ab84b978811c5ad0346d0dd9fae
Christian <macheadway@gmail.com>
2023-03-17 10:44:28 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main  - Prints whether the last digit of a random number is greater than 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar((x) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
