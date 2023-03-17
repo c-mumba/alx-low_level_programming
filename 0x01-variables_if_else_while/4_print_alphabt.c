5be792074c6b2d3cbf072bb814492de3e69b2112
Christian <macheadway@gmail.com>
2023-03-17 09:47:29 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4_print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main  - Prints whether the last digit of a random number is greater than 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' || x != 'e')
+		{
+			putchar(x);
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
