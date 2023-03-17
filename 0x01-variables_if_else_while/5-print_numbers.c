6cebe2c7516617561eb873181c4bc7ee302bb794
Christian <macheadway@gmail.com>
2023-03-17 10:28:26 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main  - Prints whether the last digit of a random number is greater than 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+		printf("%i", x);}
+	putchar('\n');
+	return (0);
+}
