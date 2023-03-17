76c8e9cbf946f9d7ec1bd8bae249288d397c1733
Christian <macheadway@gmail.com>
2023-03-17 05:49:31 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main  - Prints whether the last digit of a random number is greater than 5
+ * Return: 0
+ */
+
+int main()
+{
+    for (int x = 'a'; x <= 'z'; x++)
+    {
+        putchar(x);
+    }
+    putchar('\n');
+    return (0);
+}
