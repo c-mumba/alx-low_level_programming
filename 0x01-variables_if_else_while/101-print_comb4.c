0b77554e5c234d0d48494e436c8360adf3a596b7
Christian <macheadway@gmail.com>
2023-03-17 14:57:34 -0700

    trial

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main  - Prints whether the last digit of a random number is greater than 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int i;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (i = x + 1; i < 10; i++)
+		{
+			putchar((x) + '0');
+			putchar((i) + '0');
+			if (x == 8 && i == 9)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
