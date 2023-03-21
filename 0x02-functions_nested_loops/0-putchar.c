42980508965e4b8ae6b454a109a1d33ae459dda1
Christian <macheadway@gmail.com>
2023-03-21 00:56:09 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main  - Prints whether the last digit of a random number is greater than 5
+ * Return: 0
+ */
+
+int main()
+{
+	printf("_putchar\n");
+	return (0);
+}
