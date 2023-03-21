eff32f85da1176718a1799bf4d2d8564e6e653b6
Christian <macheadway@gmail.com>
2023-03-21 08:57:26 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - Short description, single line
+ * @n: Description of parameter x
+ * Return: 0 or 1 or -1
+ */
+
+
+int print_sign(int n)
+{
+	if( n > 0)
+	{
+		_putchar('+');
+		_putchar('\n');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar('\n');
+		return (-1);
+	}
+}
