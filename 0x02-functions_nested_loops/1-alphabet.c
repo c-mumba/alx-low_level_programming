68e50dfd93fd51575169224275cb9259da4564b8
Christian <macheadway@gmail.com>
2023-03-21 05:37:10 -0700

    trial

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <ctype.h>
+#include "main.h"
+
 /**
- * main  - Prints whether the last digit of a random number is greater than 5
+ * main  - runs print_alphabet function
+ * return 0
+ * print_alphabet  - Prints alphabet
+ * main - runs print_alphabet function
  * Return: 0
  */
 
-void print_alphabet(void);
 
 int main(void)
 {
