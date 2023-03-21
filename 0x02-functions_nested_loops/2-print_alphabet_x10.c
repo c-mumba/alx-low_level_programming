7e1328544969b5ec72c095b538fcfcac1e17723a
Christian <macheadway@gmail.com>
2023-03-21 07:31:51 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,36 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <ctype.h>
-
+#include "main.h"
 
 /**
  * main  - Prints whether the last digit of a random number is greater than 5
  * Return: 0
- *print_alphabet_x10 - print alphabet
- *return nothing
+ * print_alphabet_x10 - print alphabet
+ * return nothing
  */
 
-
-void print_alphabet_x10(void);
-int main()
-{
-	print_alphabet_x10();
-	return (0);
-}
 
 void print_alphabet_x10(void)
 {
 	int num = 0, letter;
+	
 	while (num <= 9)
 	{
 		letter = 'a';
 		while (letter <= 'z')
 		{
-			putchar(letter);
+			_putchar(letter);
 			letter++;
 		}
-		putchar('\n');
+		_putchar('\n');
 		num++;
 	}
 }
