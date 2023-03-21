020f0925d0b51dbe9e8dca3160da5a0d3b8a96b8
Christian <macheadway@gmail.com>
2023-03-21 09:20:54 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,23 +8,22 @@
 
 int print_sign(int n)
 {
-	if( n > 0)
+
+	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('1');
 		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(0);
 		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('1');
 		_putchar('\n');
 		return (-1);
 	}
