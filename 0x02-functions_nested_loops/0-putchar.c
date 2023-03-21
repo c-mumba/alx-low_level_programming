c9e3d129cc0d8686a0537280339c726d28344b18
Christian <macheadway@gmail.com>
2023-03-21 04:25:06 -0700

    Putcher

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,32 @@
-#include <main.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <ctype.h>
+#include <unistd.h>
+#include "main.h"
+
+
 /**
  * main  - Prints whether the last digit of a random number is greater than 5
  * Return: 0
+ * print_alphabet_x10 - print alphabet
+ *return nothing
  */
 
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int main()
 {
-	printf("_putchar\n");
+	char fs[] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(fs[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
+
