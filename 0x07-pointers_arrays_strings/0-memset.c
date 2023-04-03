2b9e1b7b98de288675e562a2463441c88228fcc4
Christian <macheadway@gmail.com>
2023-04-03 05:54:34 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - memset function
+ * @s: s
+ * @b: b
+ * @b: n
+ * Return: Description of the returned value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
+
