1da72788b87c86384493c5b4e5c487dfa2174281
Christian <macheadway@gmail.com>
2023-04-03 09:31:44 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - strpbrk func
+ * @haystack: haystack
+ * @needle: needle
+ * Return: Description of the returned value
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int counter, i, tester;
+
+	counter = 0;
+
+	while (haystack[counter] != '\0')
+	{
+		i = 0;
+
+		while (haystack[counter + i] == needle[i])
+		{
+			if (needle[i + 1] == '\0')
+			{
+				return (haystack + counter);
+			}
+			i++;
+		}
+		counter++;
+	}
+	return (0);
+}
