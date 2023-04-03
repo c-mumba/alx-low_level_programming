70e71ea39c569662f9ddd9049011939176cb33b7
Christian <macheadway@gmail.com>
2023-04-03 07:37:49 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - strchr function
+ * @s: s
+ * @c: c
+ * Return: Description of the returned value
+ */
+
+char *_strchr(char *s, char c)
+{
+	int counter = 0;
+
+	char *ptr;
+
+	while (s[counter] != '\0')
+	{
+		if (s[counter] == c)
+		{
+			ptr = s + counter;
+			return (ptr);
+		}
+		counter++;
+	}
+	return (NULL);
+}
