60ba9a50075a120a1897831df74a138837137458
Christian <macheadway@gmail.com>
2023-04-03 09:48:49 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,6 +10,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int counter, i, tester;
+	char *ptr;
 
 	counter = 0;
 
@@ -21,7 +22,8 @@ char *_strstr(char *haystack, char *needle)
 		{
 			if (needle[i + 1] == '\0')
 			{
-				return (haystack + counter);
+				ptr = haystack + counter;
+				return (ptr);
 			}
 			i++;
 		}
