d8fc3ea3549f03d6236bf3580f47af0f2ad50490
Christian <macheadway@gmail.com>
2023-04-10 13:33:07 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * @argv: Description of parameter x
+(* @argc: Description of parameter x
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
