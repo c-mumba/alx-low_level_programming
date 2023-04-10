8843304a59a56e7a3e6794219471f3bbe250465d
Christian <macheadway@gmail.com>
2023-04-10 14:17:07 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Short description, single line
+ * @argv: Description of parameter x
+(* @argc: Description of parameter x
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (argc * 0);
+}
