781fca77e593b11bb49a8649b07684605af8f13f
Christian <macheadway@gmail.com>
2023-04-11 11:52:10 -0700

    4. It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - Entry point
+ *@grid: paramteter
+ *@height: parameter
+ * Return: a pointer to a 2 dimensional array of integers or null
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	int *p;
+
+	for (i = 0; i < height; i++)
+	{
+		p = grid[i];
+		free(p);
+	}
+	free(grid);
+}
