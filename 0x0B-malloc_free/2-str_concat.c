24bad03a19e79b255365cc6a57962edfdac4c27c
Christian <macheadway@gmail.com>
2023-04-11 08:06:06 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,7 +29,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	len = i + j;
 
-	ptr1 = (char *)malloc(len * sizeof(char) + 2);
+	ptr1 = (char *)malloc(len * sizeof(char) + 1);
 
 	for (f = 0; f < len ; f++)
 	{
