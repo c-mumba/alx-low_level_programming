c33b224f7dbb1e6a34318cd906bd276c321c19c1
Christian <macheadway@gmail.com>
2023-04-13 12:42:07 -0700

    1. string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Short description, single line
+ * @s1: Description of parameter x
+(* @s2: Description of parameter x
+(* @n: Description of parameter x
+ * Return: Description of the returned value
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	char *ptr = malloc((sizeof(s1) + sizeof(char) * n) + 1);
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(s1) + n; i++)
+	{
+		if (i < strlen(s1))
+		{
+			if (s1[i] != '\0')
+			{
+				ptr[i] = s1[i];
+			}
+		}
+		else
+		{
+			if (s2[i - strlen(s1)] != '\0')
+			{
+				ptr[i] = s2[i - strlen(s1)];
+			}
+		}
+	}
+	ptr[strlen(s1) + n] = '\0';
+	return (ptr);
+}
