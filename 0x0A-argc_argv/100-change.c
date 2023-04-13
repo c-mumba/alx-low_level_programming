2c50d7fadfc95c4003e7dbd75d1b62ad9f01a780
Christian <macheadway@gmail.com>
2023-04-13 04:52:27 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,29 +7,28 @@
 (* @argc: Description of parameter x
  * Description: Longer description of the function)?
 (* section header: Section description)*
- * Return: Description of the returned value
+ * Return: 0 Description of the returned value
  */
 
 int main(int argc, char **argv)
 {
 	int change, counter;
 
-	counter = 0;
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	if (atoi(argv[1]) < 0)
-		return (0);
+	{
+		printf("%i\n", 0);
+		return (1);
+	}
 
-	change = atoi(argv[1]);
-	while (change > 0)
+	for (counter = 0; change > 0; counter++)
 	{
 		if (change >= 25)
-		{
 			change = change - 25;
-		}
 		else if (change >= 10)
 		{
 			change = change - 10;
@@ -50,6 +49,4 @@ int main(int argc, char **argv)
 	}
 	printf("%d\n", counter);
 	return (0);
-
 }
-
