40dc23cb248802db2f0536db31e576cd57eaf8cc
Christian <macheadway@gmail.com>
2023-04-17 16:36:21 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function with no arguments
+ * Description: prints the file name.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
