34aba579b83e2fa0f5510e89ab1da9f313da447e
Christian <macheadway@gmail.com>
2023-04-18 10:13:23 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
+
 struct dog
 {
 	char *name;
