38ac5701610109b61a34a62c829711f6fccd762a
Christian <macheadway@gmail.com>
2023-04-18 11:53:09 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 		{
-			printf("Name: nill\n");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 
 		if ((*d).owner == NULL)
 		{
-			printf("Owner: nill\n");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
