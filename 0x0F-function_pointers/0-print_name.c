dc1f3522e410ab4b0de513a0fffab17f586e46a1
Christian <macheadway@gmail.com>
2023-04-19 08:03:54 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,14 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
+/**
+ * print_name - Short description, single line
+ * @name: Description of parameter x
+(* @f: Description of parameter x
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
