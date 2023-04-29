f42c2dd7d65527b50d2677849268780639c2973e
Christian <macheadway@gmail.com>
2023-04-29 14:20:48 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - print signly likned list
+ * @head: string - head
+ * Description: singly linked list node structure
+ * Return: Description of the returned value
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp = NULL;
+
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
