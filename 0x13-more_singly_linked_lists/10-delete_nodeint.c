40f3be18e2a5a6e2c7a720dcd8302a532fbcda5f
Christian <macheadway@gmail.com>
2023-05-01 11:31:41 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	current_node = *head;
-	while (current_node->next)
+	while (current_node)
 	{
 		if (counter == index)
 		{
