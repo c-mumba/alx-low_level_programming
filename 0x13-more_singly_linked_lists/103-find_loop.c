887f724dfa0de772e55f28c3d87446ef4a2d1f38
Christian <macheadway@gmail.com>
2023-05-03 12:03:21 -0700

    103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+/**
+* find_listint_loop - finds the loop
+* @head: head
+* Return: ad
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *headd = head;
+	listint_t *tail = head;
+
+	if (!head)
+		return (NULL);
+	while (headd && tail && tail->next)
+	{
+		tail = tail->next->next;
+		headd = headd->next;
+		if (tail == headd)
+		{
+			headd = head;
+			while (headd != tail)
+			{
+				headd = headd->next;
+				tail = tail->next;
+			}
+			return (tail);
+		}
+	}
+	return (NULL);
+}
