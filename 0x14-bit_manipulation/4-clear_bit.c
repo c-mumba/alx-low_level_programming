4fe0b3f37f613046f7fc8bf540c4241ce1d3ed67
Christian <macheadway@gmail.com>
2023-05-04 14:54:52 -0700

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - Short description, single line
+ * @n: Description of parameter x
+ * @index: index
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int s, b;
+	s = sizeof(*n) * 8 - 1;
+
+	if (index > s)
+		return (-1);
+	b = 1 << index;
+	*n = *n & ~b;
+	return (1);
+}
+
